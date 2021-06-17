e392f25088425221fe8e1737809e23733160603f
Raju Saha <raj441977@gmail.com>
2021-06-17 10:43:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php
-/*echo $_SERVER['PHP_SELF'];
-echo "<br>";
-echo $_SERVER['SERVER_NAME'];
-echo "<br>";
-echo $_SERVER['HTTP_HOST'];
-echo "<br>";
-echo $_SERVER['HTTP_REFERER'];
-echo "<br>";
-echo $_SERVER['HTTP_USER_AGENT'];
-echo "<br>";
-echo $_SERVER['SCRIPT_NAME'];
-echo "<br>";
 
-$domain     = $_SERVER['HTTP_HOST'];
-echo $domain;*/
+    echo 'PHP_SELF: ' .$_SERVER['PHP_SELF'];
+    echo "<br>";
+    echo 'SERVER_NAME: ' .$_SERVER['SERVER_NAME'];
+    echo "<br>";
+    echo 'HTTP_HOST: ' .$_SERVER['HTTP_HOST'];
+    echo "<br>";
+    echo 'HTTP_REFERER: ' .$_SERVER['HTTP_REFERER'];
+    echo "<br>";
+    echo 'HTTP_USER_AGENT: ' .$_SERVER['HTTP_USER_AGENT'];
+    echo "<br>";
+    echo 'SCRIPT_NAME: ' .$_SERVER['SCRIPT_NAME'];
+    echo "<br>";
 
+    $domain     = $_SERVER['HTTP_HOST'];
+    echo $domain;
+
+/*
     header('Content-language: en-US');
     date_default_timezone_set('Asia/Kolkata');
 
@@ -30,4 +32,5 @@ echo $domain;*/
     echo $RequestURL;
     echo "<br>";
     echo $_SERVER['PHP_SELF'];
+*/
 ?>
