fbecd1fa7976af3c2161f480276f4e426ea46b0d
Raju Saha <raj441977@gmail.com>
2021-06-17 10:37:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,23 @@ echo "<br>";
 echo $_SERVER['HTTP_USER_AGENT'];
 echo "<br>";
 echo $_SERVER['SCRIPT_NAME'];
-echo "<br>";*/
+echo "<br>";
 
 $domain     = $_SERVER['HTTP_HOST'];
-echo $domain;
+echo $domain;*/
+
+    header('Content-language: en-US');
+    date_default_timezone_set('Asia/Kolkata');
+
+    session_start();
+    $domain     = $_SERVER['HTTP_HOST'];
+    $SessionID  = strtoupper(session_id());
+    $RequestURL = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+    echo $domain;
+    echo "<br>";
+    echo $SessionID;
+    echo "<br>";
+    echo $RequestURL;
+    echo "<br>";
 ?>
